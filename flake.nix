734570247741c25f91443cc536c18e2965de8057
Youser Nayme <7685106-NeuralModder@users.noreply.gitlab.com>
2025-09-09 21:48:27 +0000

    Rewrite nix flake to work with newer nix-cargo-integration
    It works now. Yay

--- flake.nix
+++ flake.nix
@@ -6,34 +6,53 @@
       flake = false;
     };
     nci = {
-      url = "github:yusdacra/nix-cargo-integration";
+      url = "github:90-008/nix-cargo-integration";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.parts.follows = "parts";
+      inputs.dream2nix.follows = "dream2nix";
+      inputs.crane.follows = "crane";
+    };
+    parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    dream2nix = {
+      url = "github:NeuralModder/dream2nix/update-crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane/v0.21.0";
+      flake = false;
     };
   };
 
-  outputs = inputs: let
-    lib = inputs.nixpkgs.lib;
-    ncl = inputs.nci.lib.nci-lib;
-
-    cleanedSrc = builtins.path {
+  outputs = inputs @ {
+    parts,
+    nci,
+    nixpkgs,
+    ...
+  }: let
+    filteredSource = builtins.path {
       name = "airshipper-source";
       path = toString ./.;
       filter = path: type:
-        lib.all
+        nixpkgs.lib.all
         (n: builtins.baseNameOf path != n)
         [
-          "rust-toolchain"
-          "rustfmt.toml"
+          ".github"
+          ".gitlab"
+          ".gitlab-ci.yml"
           "shell.nix"
           "default.nix"
-          "flake.nix"
           "flake.lock"
+          "flake.nix"
           "TROUBLESHOOTING.md"
           "CONTRIBUTING.md"
           "CHANGELOG.md"
           "CODE_OF_CONDUCT.md"
           "WORKFLOW.md"
           "PACKAGING.md"
+          "README.md"
         ];
     };
 
@@ -49,7 +68,7 @@
         echo "patching veloren-voxygen dynamic linker"
         ${pkgs.patchelf}/bin/patchelf \
           --set-interpreter "${pkgs.stdenv.cc.bintools.dynamicLinker}" \
-          --set-rpath "${lib.makeLibraryPath runtimeLibs}" \
+          --set-rpath "${nixpkgs.lib.makeLibraryPath runtimeLibs}" \
           veloren-voxygen
       '';
 
@@ -63,55 +82,87 @@
           veloren-server-cli
       '';
   in
-    inputs.nci.lib.makeOutputs {
-      root = ./.;
-      config = common: {
-        outputs.defaults = {
-          app = "airshipper";
-          package = "airshipper";
+    parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      imports = [nci.flakeModule];
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: let
+        outputs = config.nci.outputs;
+        voxygenPatcher = makeVoxygenPatcher pkgs;
+        serverPatcher = makeServerPatcher pkgs;
+        commonMkDerivation = {
+          buildInputs = with pkgs; [openssl];
+          nativeBuildInputs = with pkgs; [perl pkg-config];
         };
-        runtimeLibs = [
-          "vulkan-loader"
-          "wayland"
-          "wayland-protocols"
-          "libxkbcommon"
-          "xorg.libX11"
-          "xorg.libXrandr"
-          "xorg.libXi"
-          "xorg.libXcursor"
-        ];
-      };
-      pkgConfig = common: let
-        inherit (common) pkgs;
-        addOpenssl = prev: {
-          buildInputs = ncl.addBuildInputs prev [pkgs.openssl];
-          nativeBuildInputs = ncl.addNativeBuildInputs prev [pkgs.pkg-config];
+        serverMkDerivation = {
+          buildInputs = with pkgs; [sqlite] ++ commonMkDerivation.buildInputs;
+          nativeBuildInputs = commonMkDerivation.nativeBuildInputs;
         };
-      in {
-        airshipper.overrides = {
-          cleaned-src = {src = cleanedSrc;};
-        };
-        airshipper-server.depsOverrides = {
-          add-openssl.overrideAttrs = addOpenssl;
-        };
-        airshipper-server.overrides = {
-          add-openssl.overrideAttrs = addOpenssl;
-          cleaned-src = {src = cleanedSrc;};
-        };
-        airshipper.wrapper = _: old: let
-          voxygenPatcher = makeVoxygenPatcher pkgs;
-          serverPatcher = makeServerPatcher pkgs;
-        in
-          common.internal.pkgsSet.utils.wrapDerivation old
-          {nativeBuildInputs = [pkgs.makeWrapper];}
+        wrapPatchers = old:
+          pkgs.runCommand
+          old.name
+          {
+            meta = old.meta or {};
+            passthru =
+              (old.passthru or {})
+              // {
+                unwrapped = old;
+              };
+            nativeBuildInputs = [pkgs.makeWrapper];
+          }
           ''
-            rm -rf $out/bin
-            mkdir -p $out/bin
-            ln -sf ${old}/bin/* $out/bin
+            cp -rs --no-preserve=mode,ownership ${old} $out
             wrapProgram $out/bin/* \
-            --set VELOREN_VOXYGEN_PATCHER "${voxygenPatcher}" \
-            --set VELOREN_SERVER_CLI_PATCHER "${serverPatcher}"
+              --set VELOREN_VOXYGEN_PATCHER ${voxygenPatcher} \
+              --set VELOREN_SERVER_CLI_PATCHER ${serverPatcher} \
           '';
+        airshipper = wrapPatchers outputs."airshipper".packages.release;
+      in {
+        devShells.default = outputs."airshipper".devShell;
+        packages.default = airshipper;
+        packages.airshipper = airshipper;
+        packages.airshipper-dev = wrapPatchers outputs."airshipper".packages.dev;
+        packages.airshipper-server-dev = outputs."airshipper-server".packages.dev;
+        packages.airshipper-server-release = outputs."airshipper-server".packages.release;
+
+        nci.projects."airshipper" = {
+          export = true;
+          path = filteredSource;
+        };
+
+        nci.crates."airshipper" = {
+          export = false;
+          runtimeLibs = with pkgs;
+            [
+              libxkbcommon
+              vulkan-loader
+              wayland
+              wayland-protocols
+              xorg.libX11
+              xorg.libXrandr
+              xorg.libXi
+              xorg.libXcursor
+            ]
+            ++ commonMkDerivation.buildInputs;
+          depsDrvConfig.mkDerivation = commonMkDerivation;
+          drvConfig.mkDerivation = commonMkDerivation;
+        };
+
+        nci.crates."airshipper-server" = {
+          # Need to reexport since defining runtimeLibs here causes a strange error with tests or clippy
+          export = false;
+          runtimeLibs = serverMkDerivation.buildInputs;
+          depsDrvConfig.mkDerivation = serverMkDerivation;
+          drvConfig.mkDerivation = serverMkDerivation;
+        };
       };
     };
 }
